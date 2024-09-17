b67ed512fad8f17444efe2f5d4b8c93354107f36
BachLe27 <bachlamsung123@gmail.com>
2024-09-17 23:39:42 +0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const { default: helmet } = require("helmet");
+const compression = require('compression');
+
+const app = express()
+
+// init middlewares
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(compression())
+
+app.get('/', (req, res, next) => {
+  return res.status(200).json({
+    message: 'Welcome Back',
+  })
+})
+
+module.exports = app;
